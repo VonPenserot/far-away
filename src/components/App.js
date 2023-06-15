4898467cd94b948fa6fc83bd050dcb718ad14edc
Joachim da Silva Penserot <joachim.silva.penserot@gmail.com>
2023-06-15 08:07:22 -0400

    finished project

---
+++ src/components/App.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((currItem) => [...currItem, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((currItems) => currItems.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((currItems) =>
+      currItems.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form setItems={setItems} onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
